d03e6209f805e6697ea2464f1d8590baa112f55f
Rocky Lee <rlee52986@gmail.com>
2020-12-28 20:41:49 -0600

    navbar complete

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-.
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // Toggle Nav
+
+burger.addEventListener('click', () => {
+    // Toggle nav
+    nav.classList.toggle('nav-active');
+    
+    // Animate Links 
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('fade')
+    });
+
+}
+
+navSlide();
